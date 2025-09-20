d5bf23b682f00bc8d911cef20326ecbebe8c13b9
Amogh Saxena <amoghsaxena2004@gmail.com>
2025-09-20 12:58:41 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/lifelensai/view/AppNavigation.kt
@@ -0,0 +1,20 @@
+package com.example.lifelensai.view
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+@Composable
+fun AppNavigation(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = "splash"
+    ) {
+
+        //start pages
+        composable("splash") {
+//            SplashScreen(navController)
+        }
+    }
+}
